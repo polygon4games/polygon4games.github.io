65655fb6e5ac5ede9f3263ea8137e0a3f88042d1
polygon4games <polygon4games@gmail.com>
2016-02-16 15:02:08 -0200

    banner

--- js/util.js
+++ js/util.js
@@ -10,14 +10,3 @@ function lastModified(){
 	var date = new Date(document.lastModified);
 	document.getElementById('lastModified').innerHTML = date.toGMTString();
 }
-
-
-function sendMail(){
-
-	// var recipient="polygon4games";
-	// var at = String.fromCharCode(64);
-	// var dotcom="gmail.com";
-	// var mail="mailto:";
-	// window.open(mail+recipient+at+dotcom);
-
-}
